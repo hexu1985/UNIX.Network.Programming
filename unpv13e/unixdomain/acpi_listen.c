90a1d451fe570b707d43e92ed0d5ac3e20138454
hexu <hexu_1985@sina.com>
2021-03-05 17:30:19 +0800

    update acpi_listen.c

--- unpv13e/unixdomain/acpi_listen.c
+++ unpv13e/unixdomain/acpi_listen.c
@@ -1,41 +1,55 @@
 #include	"unp.h"
 
-void 
-acpi_cli(int sockfd, const SA *pservaddr, socklen_t servlen);
+void
+acpi_cli(int sockfd);
 
 int
 main(int argc, char **argv)
 {
 	int					sockfd;
-	struct sockaddr_un	cliaddr, servaddr;
+	struct sockaddr_un	servaddr;
 
-	sockfd = Socket(AF_LOCAL, SOCK_DGRAM, 0);
+	sockfd = Socket(AF_LOCAL, SOCK_STREAM, 0);
 
-	bzero(&cliaddr, sizeof(cliaddr));		/* bind an address for us */
-	cliaddr.sun_family = AF_LOCAL;
-	strcpy(cliaddr.sun_path, tmpnam(NULL));
-
-	Bind(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
-
-	bzero(&servaddr, sizeof(servaddr));	/* fill in server's address */
+	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sun_family = AF_LOCAL;
 	strcpy(servaddr.sun_path, "/var/run/acpid.socket");
 
-	acpi_cli(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	acpi_cli(sockfd);		/* do it all */
 
 	exit(0);
 }
 
-void 
-acpi_cli(int sockfd, const SA *pservaddr, socklen_t servlen)
+#if 0
+void
+acpi_cli(int sockfd)
 {
-	int	n;
-	char	recvline[MAXLINE + 1];
+	ssize_t		n;
+	char		buf[MAXLINE];
+
+again:
+	while ( (n = read(sockfd, buf, MAXLINE)) > 0)
+		writen(1, buf, n);
+
+	if (n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+		err_sys("str_echo: read error");
+}
+#else
+void
+acpi_cli(int sockfd)
+{
+	char	recvline[MAXLINE];
 
     for ( ; ; ) {
-		n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
 
-		recvline[n] = 0;	/* null terminate */
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
+
 		Fputs(recvline, stdout);
 	}
 }
+#endif
